f1f3e1e71aa32795a12dc5111454c30db4a6f7d2
sagar9926 <sagarkumar9926@gmail.com>
2020-11-06 09:06:55 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -22,12 +22,39 @@ class CustomDataset(Dataset):
     image = io.imread(img_path)
     #resize if needed
     if self.resize is not None:
-        image = cv2.resize(image,
-                           (self.resize[1],
-                            self.resize[0]),interpolation = cv2.INTER_AREA)
-
+        self.resize_image(image)
     y_label = torch.tensor(int(self.annotations.iloc[index,2]))
 
     if self.transform:
       image = self.transform(image)
     return(image,y_label)
+  
+  def resize_image(img, size=self.resize):
+    """
+    function takes input of any size and it creates a squared shape blank image of size image's height or width whichever is bigger.
+    it then places the original image at the center of the blank image. 
+    and then it resizes this square image into desired size so the shape of original image content gets preserved.
+    """
+
+    h, w = img.shape[:2]
+    c = img.shape[2] if len(img.shape)>2 else 1
+
+    if h == w: 
+        return cv2.resize(img, size, cv2.INTER_AREA)
+
+    dif = h if h > w else w
+
+    interpolation = cv2.INTER_AREA if dif > (size[0]+size[1])//2 else 
+                    cv2.INTER_CUBIC
+
+    x_pos = (dif - w)//2
+    y_pos = (dif - h)//2
+
+    if len(img.shape) == 2:
+        mask = np.zeros((dif, dif), dtype=img.dtype)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w] = img[:h, :w]
+    else:
+        mask = np.zeros((dif, dif, c), dtype=img.dtype)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
+
+    return cv2.resize(mask, size, interpolation)
